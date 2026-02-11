17f2f0835a665852a140694163cfeff267bd13e4
Tao Deng <dengtao1024@gmail.com>
2026-02-11 23:01:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 
-model = YOLO('/data/workspace/lhr/CMFANet/CMFANet/weights/best.pt',task='multi')  # Validate the model
+model = YOLO('/download/weights/best.pt',task='multi')  # Validate the model
 model.predict(source='test.jpg', imgsz=(640,640), device=[0],name='pre', save=True, conf=0.25, iou=0.45, show_labels=False)
